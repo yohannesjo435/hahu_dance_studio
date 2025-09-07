362cfd2482858f8039b4364e8635004a6a9d7538
yohannes <yohannesgoat@gmail.com>
2025-09-07 09:35:01 -0300

    feat: FAQ, scrollToTop-Btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,25 @@ import Hero from "@/components/Hero";
 import GallerySection from "@/components/GallerySection";
 import AboutSection from "@/components/AboutSection";
 import Company from "@/components/Company";
+import Faq from "@/components/Faq";
+import { ArrowUp } from "lucide-react";
+import Link from "next/link";
 
 const page = () => {
   return (
-    <div className="mb-36 max-w-[1500px] m-auto">
+    <div id="main" className="mb-36 max-w-[1500px] m-auto">
       <div className="mx-[2%] lg:mx-[5%]">
         <Header></Header>
         <Hero></Hero>
         <Company></Company>
         <AboutSection></AboutSection>
         <GallerySection></GallerySection>
+        <Faq></Faq>
+      </div>
+      <div className="fab">
+        <Link href="#main" className="btn btn-lg btn-circle ">
+          <ArrowUp />
+        </Link>
       </div>
     </div>
   );
---
+++ src/components/Faq.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Faq = () => {
+  return (
+    <div className="w-[80%] m-auto my-32">
+      <h2 className="text-3xl py-8 pl-1">Freqently asked Questions. </h2>
+
+      {accordion.map((item, i) => (
+        <div key={i} className="collapse bg-base-100 border border-base-300">
+          <input
+            type="radio"
+            name="my-accordion-1"
+            defaultChecked={item.open}
+          />
+          <div className="collapse-title font-semibold">{item.title}</div>
+          <div className="collapse-content text-sm">{item.text}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Faq;
+
+const accordion = [
+  {
+    title: "What if I’ve never danced before?",
+    text: "No worries at all! Our classes are designed for all levels, including complete beginners. Our instructors will guide you step-by-step, and you’ll be surprised how quickly you gain confidence and rhythm",
+    open: true,
+  },
+  {
+    title: "What styles of dance do you teach?",
+    text: "We offer a wide range: ballroom (Waltz, Tango, Foxtrot), Latin (Cha Cha, Salsa, Rumba), swing, hip hop, contemporary, and more. Whether you're looking for social dancing or competitive training, we’ve got you covered.",
+  },
+  {
+    title: " Scheduling & Enrollmen",
+    text: "No worries at all! Our classes are designed for all levels, including complete beginners. Our instructors will guide you step-by-step, and you’ll be surprised how quickly you gain confidence and rhythm",
+  },
+  {
+    title: "Do you offer classes for children?",
+    text: "Yes, we have observation windows and designated viewing days. We also host seasonal showcases so families can see their dancers shine on stage.",
+  },
+  {
+    title: "What if I’ve never danced before?",
+    text: "No worries at all! Our classes are designed for all levels, including complete beginners. Our instructors will guide you step-by-step, and you’ll be surprised how quickly you gain confidence and rhythm",
+  },
+];
